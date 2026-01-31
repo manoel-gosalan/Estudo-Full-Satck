e17ba1e9c757b22f1280a72f39bef06e634badb3
Manoel Lidio Gosalan dos Santos <manoel.gosalan@outlook.com>
2026-01-31 01:57:19 +0000

    #

--- Scripts/aula1.js
+++ Scripts/aula1.js
@@ -2,7 +2,7 @@ console.log("Olá Mundo ")
 
 console.log("Salve deu Certo");
 
-window.alert("Parabens pelo seu Primeiro Script!!!")
+window.alert("Parabens pelo seu Primeiro Script!")
 window.confirm("Voce Confirma o envio ??")
 window.prompt("Digite seu Nome: ")
 
